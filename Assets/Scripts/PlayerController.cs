2204fa6c473a2026f8493e8437a0a5cb203f19ad
will16t <60672309+will16t@users.noreply.github.com>
2021-01-06 16:54:52 -0800

    continuing to replace assets and resolve errors

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,6 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
-    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,9 +26,5 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput =  Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-        }
     }
 }
